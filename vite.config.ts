44582d1c91273bf7b8491a41303b398abe7bfdaa
kimkyungsub <cyon256@icloud.com>
2024-05-20 19:23:55 +0900

    feelings

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,15 @@
 import { defineConfig } from "vite";
+// vite.config.js
+
+import { resolve } from "path";
 export default defineConfig({
   base: "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "about/index.html"),
+      },
+    },
+  },
 });
